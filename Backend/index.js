64dd095a3e609bc62a461e4f8185233e8a4b0192
Sonu Sharma <142510804+Ssp9917@users.noreply.github.com>
2024-02-27 18:27:01 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,11 @@ connectToMongo()
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+ origin:["*"],
+ methods:["POST","GET","PUT","DELETE"],
+ credentials:true
+}))
 
 
 // awailable routes 
