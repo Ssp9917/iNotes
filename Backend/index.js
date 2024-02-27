2743f4d70630b3b674ca2134e9b0822b36233ca0
Sonu Sharma <142510804+Ssp9917@users.noreply.github.com>
2024-02-27 18:36:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,9 +12,9 @@ connectToMongo()
 // middleware
 app.use(express.json())
 app.use(cors({
- origin:["https://i-notes-frontend-ashen.vercel.app/"],
+ origin:["https://i-notes-frontend-ashen.vercel.app"],
  methods:["POST","GET","PUT","DELETE"],
- credentials:true
+ credentials: true
 }))
 
 
